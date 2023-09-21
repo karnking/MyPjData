07c4fdceace840c14fd074481f2b86d5f5c9f9a3
Ashutosh Kumawat <aurecordsofficial@gmail.com>
2023-09-21 21:48:27 +0530

    check now

--- server.js
+++ server.js
@@ -1,23 +1,10 @@
-// const jsonServer = require("json-server");
-// const server = jsonServer.create();
-// const router = jsonServer.router("db.json");
-// const middlewares = jsonServer.defaults();
-
-// server.use(middlewares);
-// server.use(router);
-// server.listen(process.env.PORT || 5000, () => {
-//   console.log("JSON Server is running");
-// });
-
-
-
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // Adjust the path to your JSON data file
+const router = jsonServer.router("db.json"); // Adjust the path to your JSON data file
 const middlewares = jsonServer.defaults();
 
 // Enable CORS
-const cors = require('cors');
+const cors = require("cors");
 server.use(cors());
 
 server.use(middlewares);
