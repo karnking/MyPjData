9727de0018ecf831bed374fc951508bb33a7038f
Karan kotai <karankotai9183@gmail.com>
2023-09-24 19:21:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,14 @@ const corsOptions ={
     credentials: true
 }
 const cors = require("cors");
-server.use(cors(corsOptions));
+server.use(cors());
+server.use(function (req, res, next) {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+   res.setHeader('Access-Control-Allow-Credentials', true);
+   next();
+});
 
 server.use(middlewares);
 server.use(router);
